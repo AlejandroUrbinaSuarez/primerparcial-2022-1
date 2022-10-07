1cb5edadacd1822f6512c876405b84a6b744ff1d
unknown <alejandroarturousua@ufps.edu.co>
2022-10-07 11:46:05 -0500

    correciones al carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,19 +1,26 @@
 function leer(){
     let cocteles = JSON.parse(localStorage.getItem("Cocteles"));
     document.getElementById("carrito").innerHTML = ""
+    let total = 0;
     for(let i=0; i<cocteles.length; i++){
         console.log(cocteles[i.nombre])
         let nombre = cocteles[i].nombre
         let precio = cocteles[i].precio
+        total += precio;
 
-        document.getElementById("carrito").innerHTML += 
-      `
+        document.getElementById("carrito").innerHTML +=  `
       <tr>
       <th scope="row">${nombre}</th>
       <td>${precio}</td>
         </tr>
       `
     }
-    leer()
+    document.getElementById("carrito").innerHTML +=  `
+    <tr>
+    <th scope="row"></th>
+    <td>${total}</td>
+      </tr>
+    `
 
     }
+    leer()
